e4892b1cf81912a79a9cd7fda32450f584cc4d8e
nicoleeeee <nifelder@tsn.at>
2023-01-09 19:23:54 +0100

    JDBC - Insert

--- jdbcdemo/src/main/java/Jdbcdemo.java
+++ jdbcdemo/src/main/java/Jdbcdemo.java
@@ -5,27 +5,57 @@ public class Jdbcdemo {
     public static void main(String[] args) {
         System.out.println("JDBC Demo!");
         selectAllDemo();
+        insertStudentDemo("Name des Studenten", "Email@prov.at");
+        selectAllDemo();
+
     }
 
-    public static void selectAllDemo() {
+    public static void selectAllDemo(){
         System.out.println("Select DEMO mit JDBC");
         String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
         String user = "root";
         String pwd = "";
 
-        try (Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
+        try(Connection conn = DriverManager.getConnection(connectionUrl,user,pwd)) {
             System.out.println("Verbindung zur DB hergestellt!");
             PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM student");
             ResultSet rs = preparedStatement.executeQuery();
-            while (rs.next()) {
+            while(rs.next()){
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String email = rs.getString("email");
                 System.out.println("Student aus der DB: ID " + id + " NAME " + name + " EMAIL " + email);
             }
-        } catch (SQLException e) {
+        } catch(SQLException e){
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+    public static void insertStudentDemo(String name, String email){
+        System.out.println("INSERT DEMO mit JDBC");
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl,user,pwd)) {
+            System.out.println("Verbindung zur DB hergestellt!");
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, ?, ?)"
+            );
+            try {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, email);
+                int rowAffected = preparedStatement.executeUpdate();
+                System.out.println(rowAffected + "Datensätze eingefügt");
+            } catch (SQLException ex) {
+                System.out.println("Fehler im SQL-INSERT Statement: " + ex.getMessage());
+            }
+
+        } catch(SQLException e){
             System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
         }
     }
 }
+
+
 
